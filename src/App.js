595f3aef322175815c97e8d16c03fbd6dd8c1b30
j3ffjessie <jejess6125@gmail.com>
2020-12-06 14:49:05 -0600

    Fix delete functionality, currying

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-import React, { useState } from "react"
-import {v4 as uuid } from 'uuid'
-import "./App.css"
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
 
+const removeLastItem = (list) =>
+  list.filter((_, index) => index !== list.length - 1);
 
+// const deleteItem = (list, itemKey) => list.filter((item) => item.key !== itemKey)
+//currying
+const deleteItem = (itemKey) => (list) =>
+  list.filter((item) => item.key !== itemKey);
 
-
-
-const removeLastItem = list => list.filter((_, index) => index !== list.length -1)
+const addItem = (itemData) => (list) => 
+  list.concat(itemData)
 
 function ShoppingList(props) {
   const [list, setList] = React.useState([]);
   const [itemName, setItemName] = React.useState("");
 
-
   function handleSubmit(event) {
     event.preventDefault();
-    setList((previousListState) => 
-    
-    previousListState.concat({key: uuid(), name: itemName })
-    )
-    setItemName("")
+    // setList((previousListState) =>
+    //   previousListState.concat({ key: uuid(), name: itemName })
+    // );
+    setList(addItem({ key: uuid(), name: itemName }));
+    setItemName("");
   }
-
   
+
   return (
     <div>
       <h1>{props.listName}</h1>
       <form>
-      <label>
-        New Item Name
-        <input
-          type="text"
-          value={itemName}
-          onChange={(e) => setItemName(e.target.value.toUpperCase())}
-        />
+        <label>
+          New Item Name
+          <input
+            type="text"
+            value={itemName}
+            onChange={(e) => setItemName(e.target.value.toUpperCase())}
+          />
         </label>
         <button onClick={handleSubmit}>Add Item</button>
       </form>
-        {/* <button onClick={() => setList(removeLastItem)}>Delete Item</button> */}
+      <button onClick={() => setList(removeLastItem)}>Delete Last Item</button>
       <ul className="ShoppingList-list">
         {list.map((listItem) => {
           return (
-            <li key={listItem} className="ShoppingList-item">
-              {listItem.name} {" "} 
-              <button onClick={() => setList(removeLastItem)}>X</button>
+            <li key={listItem.key} className="ShoppingList-item">
+              {listItem.name}{" "}
+              {/* <button onClick={() => setList(removeLastItem)}>X</button> */}
+              <button onClick={() => setList(deleteItem(listItem.key))}>X</button>
             </li>
-          )
+          );
         })}
       </ul>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -57,10 +62,11 @@ function App() {
 
   return (
     <div className="main-div">
-      <ShoppingList listName="First List for HEB"/>
-      
-      <ShoppingList listName="Second List for Whole Foods"/>
-      </div>)};
-     
+      <ShoppingList listName="First List for HEB" />
+
+      <ShoppingList listName="Second List for Whole Foods" />
+    </div>
+  );
+}
 
 export default App;
